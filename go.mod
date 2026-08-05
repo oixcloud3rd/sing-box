15ecb609452c0902de243c0b1f7deaba0d586b86
Nagase Mintao <80738880+NagaseMinato@users.noreply.github.com>
2026-08-05 21:52:26 +0800

    snell: Add ECH-TLS transport support

--- go.mod
+++ go.mod
@@ -80,6 +80,8 @@ require (
 	howett.net/plist v1.0.1
 )
 
+replace github.com/sagernet/sing-snell => github.com/oixcloud3rd/sing-snell v0.0.0-20260727102834-1cb49a5bae3f
+
 require (
 	filippo.io/edwards25519 v1.2.0 // indirect
 	filippo.io/hpke v0.4.0 // indirect
@@ -129,6 +131,7 @@ require (
 	github.com/libp2p/go-nat v1.0.1-0.20250821073202-01afc089f138 // indirect
 	github.com/libp2p/go-netroute v0.2.1 // indirect
 	github.com/mdlayher/socket v0.6.0 // indirect
+	github.com/metacubex/blake3 v0.1.0 // indirect
 	github.com/mitchellh/go-ps v1.0.0 // indirect
 	github.com/philhofer/fwd v1.2.0 // indirect
 	github.com/pierrec/lz4/v4 v4.1.26 // indirect
---
+++ protocol/snell/outbound_test.go
@@ -0,0 +1,113 @@
+package snell
+
+import (
+	"testing"
+
+	"github.com/sagernet/sing-box/option"
+
+	"github.com/stretchr/testify/require"
+)
+
+func newECHTLSOptions() option.SnellOutboundOptions {
+	return option.SnellOutboundOptions{
+		Version: 4,
+		AbstractSnellOutboundOptions: option.AbstractSnellOutboundOptions{
+			PSK: "password",
+			OutboundTLSOptionsContainer: option.OutboundTLSOptionsContainer{
+				TLS: &option.OutboundTLSOptions{
+					Enabled: true,
+					ALPN:    []string{"h2"},
+					ECH:     &option.OutboundECHOptions{Enabled: true},
+				},
+			},
+		},
+	}
+}
+
+func TestValidateECHTLSOptions(t *testing.T) {
+	t.Parallel()
+
+	legacyOptions := option.SnellOutboundOptions{
+		Version: 4,
+		AbstractSnellOutboundOptions: option.AbstractSnellOutboundOptions{
+			PSK: "password",
+		},
+	}
+	enabled, err := validateECHTLSOptions(legacyOptions)
+	require.NoError(t, err)
+	require.False(t, enabled)
+
+	validOptions := newECHTLSOptions()
+	enabled, err = validateECHTLSOptions(validOptions)
+	require.NoError(t, err)
+	require.True(t, enabled)
+
+	testCases := []struct {
+		name        string
+		modify      func(*option.SnellOutboundOptions)
+		errorString string
+	}{
+		{
+			name: "version 6",
+			modify: func(options *option.SnellOutboundOptions) {
+				options.Version = 6
+			},
+			errorString: "requires version 4",
+		},
+		{
+			name: "TLS disabled",
+			modify: func(options *option.SnellOutboundOptions) {
+				options.TLS.Enabled = false
+			},
+			errorString: "requires TLS",
+		},
+		{
+			name: "missing ECH",
+			modify: func(options *option.SnellOutboundOptions) {
+				options.TLS.ECH = nil
+			},
+			errorString: "requires ECH",
+		},
+		{
+			name: "ECH disabled",
+			modify: func(options *option.SnellOutboundOptions) {
+				options.TLS.ECH.Enabled = false
+			},
+			errorString: "requires ECH",
+		},
+		{
+			name: "obfs conflict",
+			modify: func(options *option.SnellOutboundOptions) {
+				options.ObfsOptions.ObfsMode = "http"
+			},
+			errorString: "cannot be combined with obfs",
+		},
+	}
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			options := newECHTLSOptions()
+			testCase.modify(&options)
+			enabled, err := validateECHTLSOptions(options)
+			require.False(t, enabled)
+			require.ErrorContains(t, err, testCase.errorString)
+		})
+	}
+}
+
+func TestValidateIdentityOptions(t *testing.T) {
+	t.Parallel()
+
+	require.NoError(t, validateIdentityOptions(option.SnellOutboundOptions{
+		Version: 4,
+		AbstractSnellOutboundOptions: option.AbstractSnellOutboundOptions{
+			Identity: true,
+		},
+	}))
+	require.NoError(t, validateIdentityOptions(option.SnellOutboundOptions{Version: 6}))
+	require.ErrorContains(t, validateIdentityOptions(option.SnellOutboundOptions{
+		Version: 6,
+		AbstractSnellOutboundOptions: option.AbstractSnellOutboundOptions{
+			Identity: true,
+		},
+	}), "requires version 4")
+}
