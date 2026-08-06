bfb50bb226cd06b7533e4b51ce2081cf7c778e7c
Nagase Mintao <80738880+NagaseMinato@users.noreply.github.com>
2026-08-06 14:32:50 +0800

    snell: Add ECH-TLS identity v2 and preconnect

--- protocol/snell/inbound.go
+++ protocol/snell/inbound.go
@@ -8,6 +8,7 @@ import (
 	"github.com/sagernet/sing-box/adapter"
 	"github.com/sagernet/sing-box/adapter/inbound"
 	"github.com/sagernet/sing-box/common/listener"
+	"github.com/sagernet/sing-box/common/tls"
 	"github.com/sagernet/sing-box/common/uot"
 	C "github.com/sagernet/sing-box/constant"
 	"github.com/sagernet/sing-box/log"
@@ -15,6 +16,7 @@ import (
 	snellprotocol "github.com/sagernet/sing-snell"
 	"github.com/sagernet/sing-snell/snellv5"
 	"github.com/sagernet/sing-snell/snellv6"
+	"github.com/sagernet/sing/common"
 	"github.com/sagernet/sing/common/auth"
 	E "github.com/sagernet/sing/common/exceptions"
 	F "github.com/sagernet/sing/common/format"
@@ -31,19 +33,32 @@ var _ adapter.TCPInjectableInbound = (*Inbound)(nil)
 
 type Inbound struct {
 	inbound.Adapter
-	router   adapter.ConnectionRouterEx
-	logger   logger.ContextLogger
-	listener *listener.Listener
-	service  snellprotocol.Service
-	users    []option.SnellUser
+	router    adapter.ConnectionRouterEx
+	logger    logger.ContextLogger
+	listener  *listener.Listener
+	tlsConfig tls.ServerConfig
+	tlsALPN   []string
+	service   snellprotocol.Service
+	users     []option.SnellUser
 }
 
 func NewInbound(ctx context.Context, router adapter.Router, logger log.ContextLogger, tag string, options option.SnellInboundOptions) (adapter.Inbound, error) {
+	echTLSEnabled, err := validateInboundOptions(options)
+	if err != nil {
+		return nil, err
+	}
 	inbound := &Inbound{
 		Adapter: inbound.NewAdapter(C.TypeSnell, tag),
 		router:  uot.NewRouter(router, logger),
 		logger:  logger,
 		users:   options.Users,
+	}
+	if echTLSEnabled {
+		inbound.tlsConfig, err = tls.NewServer(ctx, logger, common.PtrValueOrDefault(options.TLS))
+		if err != nil {
+			return nil, E.Cause(err, "create Snell ECH-TLS server")
+		}
+		inbound.tlsALPN = []string(options.TLS.ALPN)
 	}
 	var userList []int
 	var keyList [][]byte
@@ -55,7 +70,6 @@ func NewInbound(ctx context.Context, router adapter.Router, logger log.ContextLo
 			keyList[index] = []byte(user.UserKey)
 		}
 	}
-	var err error
 	switch options.Version {
 	case 5:
 		var obfsMode snellprotocol.ObfsMode
@@ -65,6 +79,7 @@ func NewInbound(ctx context.Context, router adapter.Router, logger log.ContextLo
 		}
 		serviceOptions := snellv5.ServiceOptions{
 			PSK:      []byte(options.PSK),
+			Identity: options.Identity,
 			ObfsMode: obfsMode,
 			Handler:  inbound,
 		}
@@ -119,18 +134,64 @@ func NewInbound(ctx context.Context, router adapter.Router, logger log.ContextLo
 	return inbound, nil
 }
 
+func validateInboundOptions(options option.SnellInboundOptions) (bool, error) {
+	if options.Identity && options.Version != 5 {
+		return false, E.New("snell: inbound identity requires version 5")
+	}
+	hasTLS := options.TLS != nil
+	if !hasTLS {
+		return false, nil
+	}
+	if options.Version != 5 {
+		return false, E.New("snell: ECH-TLS requires inbound version 5")
+	}
+	if !options.TLS.Enabled {
+		return false, E.New("snell: ECH-TLS requires TLS")
+	}
+	if options.TLS.ECH == nil || !options.TLS.ECH.Enabled {
+		return false, E.New("snell: ECH-TLS requires ECH")
+	}
+	obfsMode, err := snellprotocol.ParseObfsMode(options.ObfsOptions.ObfsMode)
+	if err != nil {
+		return false, err
+	}
+	if obfsMode != snellprotocol.ObfsModeNone {
+		return false, E.New("snell: ECH-TLS cannot be combined with obfs")
+	}
+	return true, nil
+}
+
 func (h *Inbound) Start(stage adapter.StartStage) error {
 	if stage != adapter.StartStateStart {
 		return nil
+	}
+	if h.tlsConfig != nil {
+		if err := h.tlsConfig.Start(); err != nil {
+			return err
+		}
 	}
 	return h.listener.Start()
 }
 
 func (h *Inbound) Close() error {
-	return h.listener.Close()
+	return common.Close(h.listener, h.tlsConfig)
 }
 
 func (h *Inbound) NewConnection(ctx context.Context, conn net.Conn, metadata adapter.InboundContext, onClose N.CloseHandlerFunc) {
+	if h.tlsConfig != nil {
+		tlsConn, err := tls.ServerHandshake(ctx, conn, h.tlsConfig)
+		if err != nil {
+			N.CloseOnHandshakeFailure(conn, onClose, err)
+			h.logger.ErrorContext(ctx, E.Cause(err, "process connection from ", metadata.Source, ": TLS handshake"))
+			return
+		}
+		if err = validateECHTLSConnection(tlsConn, h.tlsALPN); err != nil {
+			N.CloseOnHandshakeFailure(tlsConn, onClose, err)
+			h.logger.ErrorContext(ctx, E.Cause(err, "process connection from ", metadata.Source, ": ECH-TLS validation"))
+			return
+		}
+		conn = tlsConn
+	}
 	err := h.service.NewConnection(adapter.WithContext(ctx, &metadata), conn, metadata.Source, onClose)
 	if err != nil {
 		N.CloseOnHandshakeFailure(conn, onClose, err)
